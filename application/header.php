51d7657651b93e4ce04932becc9fc0a426bd551b
farlistener <contact@levieuxcedric.com>
2015-10-17 09:22:19 +0200

    # instead of do_login.php

--- application/header.php
+++ application/header.php
@@ -117,7 +117,7 @@ $connection = openConnection();
 					<li><a class="logoutLink" href="do_logout.php" title="<?php echo lang("menu_logout"); ?>"
 						data-toggle="tooltip" data-placement="bottom"><span class="glyphicon glyphicon-log-out"></span><span class="sr-only">Logout</span> </a></li>
 					<?php 	} else { ?>
-					<li><a id="loginLink" href="do_login.php" title="<?php echo lang("menu_login"); ?>"
+					<li><a id="loginLink" href="#" title="<?php echo lang("menu_login"); ?>"
 						data-toggle="tooltip" data-placement="bottom"><span class="glyphicon glyphicon-log-in"></span><span class="sr-only">Login</span> </a></li>
 					<?php 	}?>
 				</ul>
